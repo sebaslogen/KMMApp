7b294c98a6dd2d3a273ad8d79accf3ba5ef2dc42
sebas <sebastian@q42.nl>
2022-08-30 11:00:05 +0200

    Fix Swift code init

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,9 +2,13 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    let greet = MainViewModel().greeting()
-// 	let greeter = Greeter().greeting()
-// 	let greet = "\(greeter) \(vm)"
+    let vmGreeter: String = MainViewModel().greeting()
+ 	let greeter: String = Greeter().greeting()
+    let greet: String
+    
+    init() {
+        self.greet = "\(greeter) \(vmGreeter)"
+    }
 
 	var body: some View {
 		Text(greet)
