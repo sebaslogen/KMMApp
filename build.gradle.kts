b8253d46f5a40f1490b9c4bd714c6a80f0dab307
sebas <sebastian@q42.nl>
2022-09-30 15:00:37 +0200

    UpGradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:7.3.0")
 //        classpath("dev.icerock.moko:kswift-gradle-plugin:0.6.0")
     }
 }
